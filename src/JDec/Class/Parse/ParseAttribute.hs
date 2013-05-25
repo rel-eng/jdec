99362d11a79e5a210ea352626bb5310695a0fc65
rel-eng <rel-eng@yandex.ru>
2013-05-25 23:01:59 +0400

    Load more attributes

--- src/JDec/Class/Parse/ParseAttribute.hs
+++ src/JDec/Class/Parse/ParseAttribute.hs
@@ -2,7 +2,7 @@ module JDec.Class.Parse.ParseAttribute (
 deserializeAttribute
 ) where 
 
-import JDec.Class.Raw.Attribute (Attribute(ConstantValueAttribute))
+import JDec.Class.Raw.Attribute (Attribute(ConstantValueAttribute, SyntheticAttribute, SignatureAttribute, DeprecatedAttribute))
 import JDec.Class.Raw.ConstantPoolIndex(ConstantPoolIndex(ConstantPoolIndex))
 import JDec.Class.Raw.ConstantPoolEntry (ConstantPoolEntry(UTF8ConstantPoolEntry))
 
@@ -31,8 +31,10 @@ parseAttribute constantPool nameIndex attributeLength =
       case entry of
         UTF8ConstantPoolEntry t ->
           case (unpack t) of
-            "ConstantValue" -> do
-              if attributeLength >= 2 then parseConstantValue attributeLength else skipAttribute attributeLength
+            "ConstantValue" -> if attributeLength >= 2 then parseConstantValue attributeLength else skipAttribute attributeLength
+            "Synthetic" -> parseSynthetic attributeLength
+            "Signature" -> if attributeLength >= 2 then parseSignatureValue attributeLength else skipAttribute attributeLength
+            "Deprecated" -> parseDeprecated attributeLength
             _ -> skipAttribute attributeLength
         _ -> skipAttribute attributeLength
     Nothing -> do
@@ -46,6 +48,28 @@ parseConstantValue attributeLength = do
  constantIndex <- getWord16be
  when (attributeLength > 2) (void (getLazyByteString (attributeLength - 2)))
  return $! Just (ConstantValueAttribute (ConstantPoolIndex (toInteger constantIndex)))
+
+-- | Parse synthetic attribute-specific data
+parseSynthetic :: Int64 -- ^ Attribute length
+  -> Get (Maybe Attribute) -- ^ Attribute, if any
+parseSynthetic attributeLength = do
+ when (attributeLength > 0) (void (getLazyByteString (attributeLength)))
+ return $! Just SyntheticAttribute
+
+-- | Parse signature attribute-specific data
+parseSignatureValue :: Int64 -- ^ Attribute length
+  -> Get (Maybe Attribute) -- ^ Attribute, if any
+parseSignatureValue attributeLength = do
+ signatureIndex <- getWord16be
+ when (attributeLength > 2) (void (getLazyByteString (attributeLength - 2)))
+ return $! Just (SignatureAttribute (ConstantPoolIndex (toInteger signatureIndex)))
+
+-- | Parse deprecated attribute-specific data
+parseDeprecated :: Int64 -- ^ Attribute length
+  -> Get (Maybe Attribute) -- ^ Attribute, if any
+parseDeprecated attributeLength = do
+ when (attributeLength > 0) (void (getLazyByteString (attributeLength)))
+ return $! Just DeprecatedAttribute
 
 -- | Skip attribute
 skipAttribute :: Int64 -- ^ Attribute length
